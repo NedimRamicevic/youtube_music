e43c587b00c7c85deeddf36796c5d146c58d2bf1
Nedim <mr.ramicevic@gmail.com>
2021-12-26 10:51:11 +0300

    addStyles

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,9 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       appBar: BuildAppBar(),
+      body: BuildListInfo(),
     );
   }
 }
