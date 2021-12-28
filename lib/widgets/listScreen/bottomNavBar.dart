285fd0d6131c9cebe9c3765035da9294ffb45755
Nedim <mr.ramicevic@gmail.com>
2021-12-28 12:30:47 +0300

    navBarFuncDone

--- lib/widgets/listScreen/bottomNavBar.dart
+++ lib/widgets/listScreen/bottomNavBar.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 
-class BuildBottomNavBar extends StatelessWidget {
+class BuildBottomNavBar extends StatefulWidget {
   const BuildBottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  _BuildBottomNavBarState createState() => _BuildBottomNavBarState();
+}
+
+class _BuildBottomNavBarState extends State<BuildBottomNavBar> {
+  int _currentIndex = 0;
+  void _onTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      onTap: (index) {},
+      onTap: _onTapped,
+      currentIndex: _currentIndex,
       backgroundColor: Colors.grey[800],
       selectedItemColor: Colors.white,
       unselectedItemColor: Colors.grey,
-      items: const [
+      items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
             icon: Icon(
               Icons.home,
