52290fee8f39b48d0f14dad2a1299cd2a917cfad
Nedim <mr.ramicevic@gmail.com>
2022-01-11 10:01:10 +0300

    createHomeAppBar

---
+++ lib/models/homeAppBar.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class BuildHomeAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const BuildHomeAppBar({Key? key}) : super(key: key);
+  @override
+  Size get preferredSize => const Size.fromHeight(40);
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leadingWidth: 100,
+      backgroundColor: Colors.black,
+      leading: Row(
+        children: [
+          Stack(children: const [
+            CircleAvatar(
+              radius: 14,
+              backgroundColor: Colors.red,
+            ),
+            Positioned(
+              top: -10,
+              left: -10,
+              child: IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.play_circle_outline,
+                    color: Colors.white,
+                  )),
+            ),
+          ]),
+          const Text(
+            "Music",
+            style: TextStyle(
+                color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
+      actions: const [
+        IconButton(
+            onPressed: null,
+            icon: Icon(
+              Icons.screen_share_outlined,
+              color: Colors.white,
+            )),
+        IconButton(
+            onPressed: null,
+            icon: Icon(
+              Icons.search_sharp,
+              color: Colors.white,
+            )),
+        CircleAvatar(
+          radius: 15,
+          backgroundImage: AssetImage("assets/bergen.jpg"),
+        )
+      ],
+    );
+  }
+}
