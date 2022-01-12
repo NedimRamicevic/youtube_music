cdf0e971398c127430f6dcfe99a42a2e8ed4faed
Nedim <mr.ramicevic@gmail.com>
2022-01-12 10:07:44 +0300

    createLists

--- lib/widgets/exploreScreen/explore.dart
+++ lib/widgets/exploreScreen/explore.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_music/models/homeAppBar.dart';
+import 'package:youtube_music/models/listCard.dart';
 import 'package:youtube_music/widgets/bottomNavBar.dart';
 import 'package:youtube_music/widgets/exploreScreen/opList.dart';
 
@@ -10,14 +11,41 @@ class Explore extends StatefulWidget {
   _ExploreState createState() => _ExploreState();
 }
 
+List<ListCard> generateCardList() {
+  List<ListCard> listCard = [
+    const ListCard(
+        title: "RELEASED",
+        artistList: ["Bergen", "Tüdanya", "Müslüm"],
+        coverPhoto: "assets/bergen.jpg"),
+    const ListCard(
+        title: "MOST POPULAR",
+        artistList: ["Aurora"],
+        coverPhoto: "assets/dangerous.JPEG"),
+    const ListCard(
+        title: "OLD",
+        artistList: ["Bergen", "Tüdanya", "Müslüm"],
+        coverPhoto: "assets/bergen2.jpg"),
+  ];
+  return listCard;
+}
+
 class _ExploreState extends State<Explore> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const BuildHomeAppBar(),
       body: Container(
+        color: Colors.black,
         child: Column(
-          children: [OpList()],
+          children: const [
+            OpList(),
+            Padding(
+              padding: EdgeInsets.fromLTRB(25, 0, 25, 0),
+              child: Divider(
+                color: Colors.grey,
+              ),
+            )
+          ],
         ),
       ),
       bottomNavigationBar: const BuildBottomNavBar(),
